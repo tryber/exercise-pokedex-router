7b71a355371344f5e1c39d131e5782efb48a81f0
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-06-07 19:20:02 -0300

    immporta BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <h1> Pokedex </h1>
       <Pokedex pokemons={pokemons} />
+      </BrowserRouter>
     </div>
   );
 }
